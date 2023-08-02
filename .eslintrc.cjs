898bec4a7620038a4ec2eab558b50b141b9c7953
thinknathan <github@thinknathan.com>
2023-08-01 17:17:49 -0700

    Simplify lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,41 +6,20 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
-	globals: {
-		wp: true,
-	},
 	env: {
 		node: true,
 		es6: true,
 		amd: true,
 		browser: true,
-		jquery: true,
 	},
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: 2018,
+		ecmaVersion: 2020,
 		sourceType: 'module',
 	},
 	plugins: ['import', '@typescript-eslint', 'prettier'],
-	settings: {
-		'import/core-modules': [],
-		'import/ignore': [
-			'node_modules',
-			'\\.(coffee|scss|css|less|hbs|svg|json)$',
-		],
-	},
 	rules: {
 		'prettier/prettier': 'error',
 		'no-console': 0,
-		'comma-dangle': [
-			'error',
-			{
-				arrays: 'always-multiline',
-				objects: 'always-multiline',
-				imports: 'always-multiline',
-				exports: 'always-multiline',
-				functions: 'ignore',
-			},
-		],
 	},
 };
